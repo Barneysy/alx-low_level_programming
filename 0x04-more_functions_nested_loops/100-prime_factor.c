6a6175368e04c3c437c38746786bc7bffc5db7aa
Barneysy <lightpain32@gmail.com>
2023-02-26 23:43:15 +0300

    again

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+/**
+ * check_prime - checks if a number is prime
+ *
+ * @number - the number to be checked
+ *
+ * Return: 1 if the number is prime, 0 otherwise
+ */
+
+int check_prime(int number)
+{
+	int i;
+	int counter = 0;
+
+	for (i = 2; i <= number; i++)
+	{
+		if (number % i == 0)
+		{
+			counter++;
+		}
+	}
+	if (counter >= 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
+/**
+ *main - finds and prints the larges prime factor of the number
+ *@void: no arguments
+ *Return: zero
+ */
+int main(void)
+{
+	long int big_number, num, largest_prime;
+	big_number = 612852475143;
+	largest_prime = 1;
+
+	for (num = 2; num <= big_number; num++)
+	{
+		if (big_number %  num == 0)
+		{
+			if (check_prime(num))
+			{
+				largest_prime = num;
+			}
+		}
+	}
+	printf("%ld\n", largest_prime);
+}	
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *more_numbers - prints 10 times the numbers fron 0 to 14
+ *
+ * pputchar - custom int printer using _putchar
+ */
+void pputchar(int num)
+{
+	if (num / 10)
+	{
+		pputchar(num / 10);
+	}
+
+	_putchar((num % 10) + '0');
+}
+void more_numbers(void)
+{
+	int counter, num;
+
+	counter = 1;
+	while (counter <=10)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			pputchar(num);
+		}
+		_putchar('\n');
+		counter++;
+	}
+	
+}
+
